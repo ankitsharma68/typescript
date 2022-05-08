812ffb29a0f71e3daf4a9959bb0548198f853cba
ankitsharma68 <102722878+ankitsharma68@users.noreply.github.com>
2022-05-08 20:28:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,5 +57,5 @@ app.get("/:id", async (req, res) => {
   }
 })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
